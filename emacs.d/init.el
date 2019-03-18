a03b9a149dd002913cb36c5fe291f7edc2abc794
Hans Bugge Grathwohl <hans.bugge@deondigital.com>
2019-03-18 14:14:45 +0100

    C-c + bound to increment-number-at-point

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -172,6 +172,25 @@
   (interactive)
   (hans/switch-theme 'solarized-dark))
 
+;; https://www.emacswiki.org/emacs/IncrementNumber
+(defun increment-number-at-point (&optional arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+
+(global-set-key (kbd "C-c +") 'increment-number-at-point)
+
 ;; Solarized color theme
 (use-package solarized-theme
   :ensure t
